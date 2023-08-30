08368bfc9b52705dcd8716bdec7164b4e24eb5ac
7riatsu <atsuki02jojo@gmail.com>
2023-08-31 01:05:19 +0900

    use fetcher class in run.rb

--- bin/run.rb
+++ bin/run.rb
@@ -3,12 +3,13 @@
 require_relative '../lib/fetcher'
 
 ARGV.each do |url|
-  result = fetch(url)
+  fetcher = Fetcher.new(url)
+  result = fetcher.fetch
 
   if result
     puts "site: #{result[:site]}"
     puts "num_links: #{result[:num_links]}"
-    puts "images: #{result[:images]}"
+    puts "num_images: #{result[:num_images]}"
     puts "last_fetched_at: #{result[:last_fetched_at]}"
   else
     puts "Error fetching #{url}"
